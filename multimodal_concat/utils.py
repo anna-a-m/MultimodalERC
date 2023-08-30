4008f7c1d391c53db6dfa9799e04b9903a2361a1
moon-strider <iamthekidyoudoknowwhatimean@gmail.com>
2023-08-30 12:00:27 +0300

    pickle weird error fix 2

--- multimodal_concat/utils.py
+++ multimodal_concat/utils.py
@@ -122,10 +122,10 @@ def prepare_models(num_labels, model_path, device='cuda'):  # TODO: add paths to
         num_labels=num_labels
     )
     save_name = f'{model_path}bert-large-uncased_none_seed-42.pt'
-    state_dict = torch.load(save_name, pickle_module=pickle)
+    state_dict = torch.load(save_name, map_location=lambda storage, loc: storage, pickle_module=pickle)
     
     # fix keys unmatching
-    state_dict = torch.load(save_name, pickle_module=pickle)
+    state_dict = torch.load(save_name, map_location=lambda storage, loc: storage, pickle_module=pickle)
     state_dict.popitem(last=False)
     
     text_base_model.load_state_dict(state_dict)
@@ -134,7 +134,7 @@ def prepare_models(num_labels, model_path, device='cuda'):  # TODO: add paths to
     # VIDEO
     video_base_model = XCLIPClassificaionModel(num_labels)
     save_name = f'{model_path}XCLIP_Augmented.pt'
-    state_dict = torch.load(save_name, pickle_module=pickle)
+    state_dict = torch.load(save_name, map_location=lambda storage, loc: storage, pickle_module=pickle)
     
     # fix keys unmatching
     emb = state_dict.popitem(last=False)
@@ -147,7 +147,7 @@ def prepare_models(num_labels, model_path, device='cuda'):  # TODO: add paths to
     # AUDIO
     audio_base_model = ConvNet(num_labels)
     save_name = f'{model_path}1d_cnn_with_opensmile.pt'
-    checkpoint = torch.load(save_name, pickle_module=pickle)
+    checkpoint = torch.load(save_name, map_location=lambda storage, loc: storage, pickle_module=pickle)
     audio_base_model.load_state_dict(checkpoint['model_state_dict'])
     audio_model = AudioClassificationModel(audio_base_model, device=device)
 
